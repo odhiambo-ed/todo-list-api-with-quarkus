c798d932dd648f645b2891588f32ff5536410f0f
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-07 19:39:25 +0300

    add get todos

--- src/main/java/org/acme/TodoResource.java
+++ src/main/java/org/acme/TodoResource.java
@@ -1,8 +1,21 @@
 package org.acme;
 
+import io.quarkus.arc.lookup.LookupIfProperty.List;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/todos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class TodoResource {
-    
+    // TODO Auto-generated
+    private List<Todo> todos = new ArrayList<>();
+
+    @GET
+    public List<Todo> getTodos() {
+        return todos;
+    }
 }
