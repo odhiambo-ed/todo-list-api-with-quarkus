a42e4c05ffab36e0a828489e4131b8e97100c03c
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-07 19:41:40 +0300

    add Todos CRUD Methods

--- src/main/java/org/acme/TodoResource.java
+++ src/main/java/org/acme/TodoResource.java
@@ -1,9 +1,15 @@
 package org.acme;
 
+import java.util.ArrayList;
+
 import io.quarkus.arc.lookup.LookupIfProperty.List;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -15,7 +21,31 @@ public class TodoResource {
     private List<Todo> todos = new ArrayList<>();
 
     @GET
-    public List<Todo> getTodos() {
+    public List<Todo> listTodos() {
         return todos;
     }
+
+    @POST
+    public Todo addTodo(Todo todo) {
+        // Generate a unique ID for the todo (e.g., UUID)
+        // Add the todo to the list
+        todos.add(todo);
+        return todo;
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Todo updateTodo(@PathParam("id") String id, Todo updatedTodo) {
+        // Find and update the todo with the given ID
+        // Return the updated todo
+        return updatedTodo;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteTodo(@PathParam("id") String id) {
+        // Remove the todo with the given ID from the list
+    }
+
+
 }
